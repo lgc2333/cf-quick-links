7c1c6400f9ca98865604636761ac5a693bc75536
LgCookie <lgc2333@126.com>
2025-08-09 16:27:23 +0800

    up

---
+++ src/data.ts
@@ -0,0 +1,5 @@
+export const defaultRedirect = 'https://lgc2333.top'
+
+export const redirects: Record<string, string> = {
+  ab: 'https://blog.lgc2333.top/about',
+}
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,10 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { defaultRedirect, redirects } from './data'
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		const url = new URL(request.url);
-		switch (url.pathname) {
-			case '/message':
-				return new Response('Hello, World!');
-			case '/random':
-				return new Response(crypto.randomUUID());
-			default:
-				return new Response('Not Found', { status: 404 });
-		}
-	},
-} satisfies ExportedHandler<Env>;
+  async fetch(request, _env, _ctx): Promise<Response> {
+    const url = new URL(request.url)
+    const path = url.pathname.slice(1).split('/')[0]
+    if (path in redirects) return Response.redirect(redirects[path], 301)
+    return Response.redirect(defaultRedirect, 301)
+  },
+} satisfies ExportedHandler<Env>
